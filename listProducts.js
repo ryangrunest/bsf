a681ebfd7d0c6f17da1ab2954f3b26abc75876f7
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-23 06:59:51 -0700

    faker creates product names

--- listProducts.js
+++ listProducts.js
@@ -1,1 +1,9 @@
 const faker = require('faker');
+
+let productArr = [];
+
+for (var i = 0; i < 10; i++) {
+  productArr.push(faker.commerce.productName());
+  // console.log(productArr[i]);
+}
+console.log(productArr)
