80b39dc6bfe7ff785ce9850085b1163584d9fda1
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-23 07:03:01 -0700

    prices added

--- listProducts.js
+++ listProducts.js
@@ -3,7 +3,9 @@ const faker = require('faker');
 let productArr = [];
 
 for (var i = 0; i < 10; i++) {
-  productArr.push(faker.commerce.productName());
+  let name = faker.commerce.productName();
+  let price = faker.commerce.price();
+  productArr.push(`${name}: $${price}`);
   // console.log(productArr[i]);
 }
 console.log(productArr)
